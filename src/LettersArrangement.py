082a98011090d2dd8c13d470cbfdf01a1b82f5af
Caroline <carolineven29@gmail.com>
2018-08-04 13:26:12 +1000

    Create method to print all arrangements of a word

--- src/LettersArrangement.py
+++ src/LettersArrangement.py
@@ -11,20 +11,75 @@ def get_letters_count(word):
     return letters_count
 
 
+def count_permutations(word):
+    return math.factorial(len(word))
+
+
 def count_different_arrangements(word):
     letters_count = get_letters_count(word)
-    den = 1
+    identical_permutations_div = 1
     for letter in letters_count:
-        den *= math.factorial(letters_count[letter])
-    return math.factorial(len(word)) / den
+        identical_permutations_div *= math.factorial(letters_count[letter])
+    return count_permutations(word) / identical_permutations_div
 
 
-def print_arrangements(word):
+def remove_one_occurence(letter, letters_count):
+    dic = dict(letters_count)
+    if letter in dic:
+        if dic[letter] > 1:
+            dic[letter] -= 1
+        else:
+            del dic[letter]
+    return dic
+
+
+def print_letters_arrangements(word, letters_count):
+    if len(letters_count) == 0:
+        print word
+    else:
+        for letter in letters_count:
+            print_letters_arrangements(
+                word + letter,
+                remove_one_occurence(letter, letters_count)
+            )
+
+
+def print_all_arrangements(word):
+    if len(word) == 0:
+        return
+    if len(word) == 1:
+        print word
+        return
+    letters_count = get_letters_count(word)
+    print_letters_arrangements('', letters_count)
+
+
+def print_letters_permutations(first, rest):
+    if len(rest) == 0:
+        print first
+    else:
+        [print_letters_permutations(first + letter, rest.replace(letter, '', 1)) for letter in rest]
+
+
+def print_all_permutations(word):
+    if len(word) == 0:
+        return
+    if len(word) == 1:
+        print word
+        return
+    print_letters_permutations('', word)
+
+
+def print_arrangements_count(word):
     print "There are " + str(count_different_arrangements(word)) + " different arrangements of letters in word " + word
 
 
-print_arrangements("BOOKKEEPER")
-print_arrangements("ABCD")
-print_arrangements("AAA")
-print_arrangements("MISSISSIPPI")
+print_arrangements_count("BOOKKEEPER")
+print_arrangements_count("MISSISSIPPI")
+print_arrangements_count("AAA")
+
+print_arrangements_count("AABBCC")
+print_all_arrangements("AABBCC")
+
+
 
